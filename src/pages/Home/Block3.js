ffe747b78604533a6086c38cb3e904d4556f4ab0
Aleksei2576 <118676730+Aleksei2576@users.noreply.github.com>
2025-10-04 01:48:21 +0300

    Update Block3.js

--- src/pages/Home/Block3.js
+++ src/pages/Home/Block3.js
@@ -6,8 +6,8 @@ const Block3 = () => {
 			<h2 className = 'center'>Nova - the company with human face</h2>
 				<div className = 'block3Body'>
 					<div className = 'founders'>
-						<p>Alex, with more than 10 years expirience in stretch systems.
-						I look at the problem from different angles and offer solutions beyond the usual ideas</p>
+						<p>Alex, with more than 10 years expirience in stretch systems.<br/>
+						"I look at the problem from different angles and offer solutions beyond the usual ideas"</p>
 					</div>
 					<div className = 'alex commonImg'>
 						<div className = 'names'>Alex</div>
